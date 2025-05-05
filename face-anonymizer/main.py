79d8d597e85e75419309f2612c932a15b171d7f8
jessia <sadiajessia@gmail.com>
2025-05-05 22:41:34 +0600

    face anonymizer for image, video and webcam

--- face-anonymizer/main.py
+++ face-anonymizer/main.py
@@ -1,24 +1,12 @@
 # pylint: disable=no-member
 import os
+import argparse
 import cv2
 import mediapipe as mp
 
 
-output_dir = './data'
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-
-# read image
-img_path = 'data/face_img.jpeg'
-img = cv2.imread(img_path)
-img = cv2.resize(img, (800, 600))
-
-H, W, _ = img.shape
-
-# detect faces
-mp_face_detection = mp.solutions.face_detection
-
-with mp_face_detection.FaceDetection(model_selection = 0, min_detection_confidence = 0.5) as face_detection:
+def process_img(img, face_detection):
+    H, W, _ = img.shape
     img_rbg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     out = face_detection.process(img_rbg)
 
@@ -37,11 +25,63 @@ with mp_face_detection.FaceDetection(model_selection = 0, min_detection_confiden
             # img = cv2.rectangle(img, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 5)
 
             # blur faces
-            img[y1: y1+h, x1: x1+w, :] = cv2.blur(img[y1: y1+h, x1: x1+w, :], (50, 50) )
-
-    cv2.imshow('img', img)
-    cv2.waitKey(0)
+            img[y1: y1+h, x1: x1+w, :] = cv2.blur(img[y1: y1+h, x1: x1+w, :], (50, 50))
+    return img
 
 
-# save image
-cv2.imwrite(os.path.join(output_dir, 'output.jpeg'), img)
+args = argparse.ArgumentParser()
+
+args.add_argument('--mode', default = 'webcam')
+args.add_argument('--filepath', default = None)
+
+args = args.parse_args()
+
+output_dir = './data'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# detect faces
+mp_face_detection = mp.solutions.face_detection
+
+with mp_face_detection.FaceDetection(model_selection = 0, min_detection_confidence = 0.5) as face_detection:
+    
+    if args.mode in ['image']:
+        # read image
+        img = cv2.imread(args.filepath)
+        img = cv2.resize(img, (800, 600))
+        img = process_img(img, face_detection)
+
+        # save image
+        cv2.imwrite(os.path.join(output_dir, 'output.jpeg'), img)
+
+    elif args.mode in ['video']:
+
+        cap = cv2.VideoCapture(args.filepath)
+        ret, frame = cap.read()
+
+        output_video = cv2.VideoWriter(os.path.join(output_dir, 'output.mp4'), cv2.VideoWriter_fourcc(*'mp4v'), 30, (frame.shape[1], frame.shape[0]))
+
+        while ret:
+            frame = process_img(frame, face_detection)
+            output_video.write(frame)
+            ret, frame = cap.read()
+
+        cap.release()
+        output_video.release()
+
+    elif args.mode in ['webcam']:
+
+        cap = cv2.VideoCapture(0)
+        ret, frame = cap.read()
+
+        while ret:
+            frame = process_img(frame, face_detection)
+            cv2.imshow('frame', frame)
+            cv2.waitKey(30)
+
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
